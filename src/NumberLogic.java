481b37135733e5a4c19ae3c105409f102f58f7ff
timvetinte <tim.grundstrom@gmail.com>
2025-10-29 12:21:34 +0100

    start of game logic

---
+++ src/NumberLogic.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class NumberLogic {
+
+    ArrayList<Integer> tiles = new ArrayList<>();
+
+    public void startTiles(){
+        tiles.clear();
+        tiles.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,0));
+        Collections.shuffle(tiles);
+        for(Integer i : tiles){
+            System.out.println(i);
+        }
+    }
+
+    public void numericalOrder(){
+
+
+
+
+    }
+
+}
